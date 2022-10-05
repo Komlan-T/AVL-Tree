d224b67f485a6ae783a341cfc61e0211788eaf4e
Komlan-T <100947019+Komlan-T@users.noreply.github.com>
2022-10-05 19:54:59 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#include <string>
+#include <sstream>
+#include <stdexcept>
 #include "AVL.h"
 
 /* Note:
@@ -7,43 +8,57 @@
 	2. You will submit this main.cpp file and any header files you have on Gradescope.
 */
 
-bool validStudent(string name, string ID){
+bool validName(string name) {
 
-	int j = 65;
-	int k = 48;
+	name.erase(0, 1);
+	name.pop_back();
+
 	int nameCount = 0;
-	int IDCount = 0;
 
-	if(ID.length() != 8){
-		return false;
-	}
+	for (int i = 0; i < name.length(); i++) {
+		for (int j = 65; j <= 122; j++) {
 
-	for(int i = 0; i < name.length(); i++){
-		while(j <= 122){
-			if(name[i] == char(j) || name[i] == char(32)){
+			if (name[i] == char(j) || name[i] == ' ') {
 				nameCount++;
 			}
-			j++;
+
+			if (nameCount == name.length()) {
+				return true;
+			}
 		}
 	}
 
-	for(int i = 0; i < ID.length(); i++){
-		while(k <= 57){
-			if(ID[i] == char(k)){
-				IDCount++;
-			}
-			k++;
-		}
-	}
-	
-	if(IDCount == ID.length() && nameCount == name.length()){
-		return true;
-	}
 	cout << "unsuccessful" << endl;
 	return false;
 }
 
-Student makeStudent(string name, string _ID){
+bool validID(string ID) {
+
+	int IDCount = 0;
+
+	if (ID.length() != 8) {
+		cout << "unsuccessful" << endl;
+		return false;
+	}
+
+	for (int i = 0; i < ID.length(); i++) {
+		for (int j = 48; j <= 57; j++) {
+
+			if (ID[i] == char(j)) {
+				IDCount++;
+			}
+			if (IDCount == ID.length()) {
+				return true;
+			}
+		}
+
+	}
+
+	cout << "unsuccessful" << endl;
+	return false;
+}
+
+Student makeStudent(string name, string _ID) {
 
 	int ID = stoi(_ID);
 	return Student(name, ID);
@@ -52,9 +67,70 @@ Student makeStudent(string name, string _ID){
 int main() {
 
 	AVL tree;
-	tree.insert(makeStudent("Adam Stewart", "23847510"));
-	//tree.insert(makeStudent("Faith Goodwin", "47103842"));
-	//tree.insert(makeStudent("John Proctor", "35268629"));
+
+	string _run;
+	cin >> _run;
+	int run = stoi(_run);
+
+	for (int i = 0; i <= run; i++) {
+
+		string line;
+		getline(cin, line);
+
+		istringstream in(line);
+
+		string command;
+		in >> command;
+
+		if (command == "insert") {
+			string name;
+			in >> name;
+			string ID;
+			in >> ID;
+			
+			if (validName(name) && validID(ID)) {
+
+				name.erase(0, 1);
+				name.pop_back();
+				tree.insert(makeStudent(name, ID));
+			}
+		}
+		else if (command == "remove") {
+			string next;
+			in >> next;
+			if (validID(next)) {
+				tree.remove(stoi(next));
+			}
+		}
+		else if (command == "search") {
+			string next;
+			in >> next;
+			try {
+				int convert = stoi(next);
+				tree.searchID(convert);
+			}
+			catch (invalid_argument) {
+				tree.searchName(next);
+			}
+		}
+		else if (command == "printInorder") {
+			tree.printInorder();
+		}
+		else if (command == "printPreorder") {
+			tree.printPreorder();
+		}
+		else if (command == "printPostorder") {
+			tree.printPostorder();
+		}
+		else if (command == "printLevelCount") {
+			tree.printLevelCount();
+		}
+		else if (command == "removeInorder") {
+			string next;
+			in >> next;
+			tree.removeInorder(stoi(next));
+		}
+	}
+
 	return 0;
 }
-
